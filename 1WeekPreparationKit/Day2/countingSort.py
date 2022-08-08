34da66e747fec50b175cb83e538ea65926f2d42d
Sena <senabahadiroglu0@gmail.com>
2022-08-08 16:13:58 +0300

    countingSort

--- 1WeekPreparationKit/Day2/countingSort.py
+++ 1WeekPreparationKit/Day2/countingSort.py
@@ -14,3 +14,44 @@ for i in arr:
     count[i] +=1
 
 print(count)
+
+
+# -------------------------------HackerRank----------------
+
+# #!/bin/python3
+
+# import math
+# import os
+# import random
+# import re
+# import sys
+
+# #
+# # Complete the 'countingSort' function below.
+# #
+# # The function is expected to return an INTEGER_ARRAY.
+# # The function accepts INTEGER_ARRAY arr as parameter.
+# #
+
+# def countingSort(arr):
+    
+#     liste = [0]*100
+    
+#     for i in arr:
+#         liste[i]+=1
+        
+#     return liste
+
+# if __name__ == '__main__':
+#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+#     n = int(input().strip())
+
+#     arr = list(map(int, input().rstrip().split()))
+
+#     result = countingSort(arr)
+
+#     fptr.write(' '.join(map(str, result)))
+#     fptr.write('\n')
+
+#     fptr.close()
